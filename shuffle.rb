1ddc5a460f17ae56e3b6b24763f888f1573fd446
Rômulo Machado <romulo.machado5@gmail.com>
2014-12-01 01:14:55 -0200

    Remove unnecessary variable and return list joined

--- shuffle.rb
+++ shuffle.rb
@@ -1,11 +1,10 @@
 require 'sinatra'
 require 'httparty'
 require 'yaml'
+require 'pry'
 
 data = YAML.load_file(File.expand_path("./data.yml"))
 
 post('/') do
-  team = data['team_members'].shuffle
-
-  HTTParty.post(data['url'], body: { text: team, channel: "\##{params['channel_name']}" }.to_json )
+  HTTParty.post(data['url'], body: { text: data['team_members'].shuffle.join(', '), channel: "\##{params['channel_name']}" }.to_json )
 end
