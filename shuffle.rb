b07efc3a598e1d1f139ea54f776d7d1589c1249e
Rômulo Machado <romulo.machado5@gmail.com>
2014-12-02 11:08:04 -0200

    Customize default appearance

--- shuffle.rb
+++ shuffle.rb
@@ -5,5 +5,5 @@ require 'yaml'
 data = YAML.load_file(File.expand_path("./data.yml"))
 
 post('/') do
-  HTTParty.post(data['url'], body: { text: data['team_members'].shuffle.join(', '), channel: "\##{params['channel_name']}" }.to_json )
+  HTTParty.post(data['url'], body: { text: data['team_members'].shuffle.join(', '), channel: "\##{params['channel_name']}", icon_emoji: ':twisted_rightwards_arrows:', username: 'slack-shuffle' }.to_json )
 end
